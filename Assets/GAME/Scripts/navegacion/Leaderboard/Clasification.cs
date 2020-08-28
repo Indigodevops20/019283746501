e04434c32440071086b667626a324da414972dab
Brayan Izq <indigodevops@gmail.com>
2020-08-28 11:42:01 -0500

    Core assets

---
+++ Assets/GAME/Scripts/navegacion/Leaderboard/Clasification.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clasification : MonoBehaviour
+{
+    public Text PlayerName;
+    public Text PlayerLevel;
+
+}
---
+++ Assets/GAME/Scripts/navegacion/Leaderboard/Leaderboard.cs
@@ -0,0 +1,43 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public GameObject leaderboardPANEL;
+    public GameObject listingPrefab;
+    public Transform listingContainer;
+    public void GetLeaderBoard()
+    {
+        var requestLeaderBoard = new GetLeaderboardRequest { StartPosition = 0, StatisticName = "PlayerLevel", MaxResultsCount = 10 };
+        PlayFabClientAPI.GetLeaderboard(requestLeaderBoard, OnGetLeaderBoard, OnErrorLeaderboard );
+
+    }
+
+    void OnGetLeaderBoard(GetLeaderboardResult result)
+    {
+        foreach (PlayerLeaderboardEntry player in result.Leaderboard)
+        {
+            leaderboardPANEL.SetActive(true);
+            GameObject tempListing = Instantiate(listingPrefab, listingContainer);
+            Clasification LL = tempListing.GetComponent<Clasification>();
+            LL.PlayerName.text = player.DisplayName;
+            LL.PlayerLevel.text = player.StatValue.ToString();
+            Debug.Log(player.DisplayName + ": " + player.StatValue);
+        }
+    } 
+
+    void OnErrorLeaderboard(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    public void CloseLeaderBoard()
+    {
+        leaderboardPANEL.SetActive(false);
+        for (int i = listingContainer.childCount -1; i >= 0; i--)
+        {
+            Destroy(listingContainer.GetChild(i).gameObject);
+        }
+    }
+}
