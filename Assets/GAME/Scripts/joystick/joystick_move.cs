05c32f9559d81d2a74ef5950ee6f557f19f5ede4
Franco B <indigodevops@gmail.com>
2020-08-30 03:26:43 -0500

    Core assets

---
+++ Assets/GAME/Scripts/joystick/joystick_move.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class joystick_move : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Image joystickImg;
+    private Vector2 InputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+            InputVector = new Vector2(pos.x, pos.y);
+            InputVector = (InputVector.magnitude > 1.0f) ? InputVector.normalized : InputVector;
+
+            //move dot of Joystick
+            joystickImg.rectTransform.anchoredPosition = new Vector3(InputVector.x * (bgImg.rectTransform.sizeDelta.x / 2), InputVector.y * (bgImg.rectTransform.sizeDelta.y / 2));
+        }
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        InputVector = Vector2.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector2.zero;
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public float Horizontal()
+    {
+        if (InputVector.x != 0)
+        {
+            return InputVector.x;
+        }
+        else
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+    public float Vertical()
+    {
+        if (InputVector.y != 0)
+        {
+            return InputVector.y;
+        }
+        else
+        {
+            return Input.GetAxis("Vertical");
+        }
+    }
+}
--- Assets/GAME/Scripts/pets/PlayerController.cs
+++ Assets/GAME/Scripts/pets/PlayerController.cs
@@ -7,12 +7,22 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb2d;
     SpriteRenderer spr;
     Animator anim;
+    //con fricción hará que el Circle collider del jugador no se deslice para nada
     public PhysicsMaterial2D withFriction;
+    //sin fricción hará que el Circle collider del jugador se pueda deslizar
     public PhysicsMaterial2D noFriction;
-    //
+
+    //Mirando a la derecha
     bool facingRight = false;
     //velocidad
     public float maxSpeed;
+    //Suelo
+    public bool isGround;
+    public LayerMask groundLayer;
+    //Salto
+    public float jumpPower;
+    //Joystick
+    public joystick_move jm;
 
     // Start is called before the first frame update
     void Start()
@@ -22,28 +32,73 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal");
+        //ejecutamos el movimiento
+        playerMove();
+        //ejecutamos el salto
+        playerJump();
+
+    }
+
+    void Flip()
+    {
+        //hacemos que el sprite y la variable facingRight cambien a true o false
+        facingRight = !facingRight;
+        spr.flipX = !spr.flipX;
+    }
+    void playerMove()
+    {
+        //PC
+        //float move = Input.GetAxis("Horizontal");
+        //Android
+        //el movimiento se igualara al movimiento que generará el script joystick_move
+        float move = jm.Horizontal();
 
         if (move > 0 && !facingRight)
         {
             Flip();
-        }else if (move < 0 && facingRight)
+        }
+        else if (move < 0 && facingRight)
         {
             Flip();
         }
 
         rb2d.sharedMaterial = (move != 0) ? noFriction : withFriction;
 
+        //la velocidad del pet se igualara al movimiento del joystick x la variable velocidad maxima.
         rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+        //convierte el flotante en un numero absoluto(no dara numeros negativos, solo positivos) para que se lo devuelva a el parametro "moveSpeed" de la animación
         anim.SetFloat("moveSpeed", Mathf.Abs(move));
     }
-
-    void Flip()
+    void playerJump()
     {
-        facingRight = !facingRight;
-        spr.flipX = !spr.flipX;
+        //El Raycast sirve para detectar el suelo
+
+        //posicion del pet para el Raycast
+        Vector2 position = transform.position;
+        //la dirección hacia donde se dibujara el Raycast
+        Vector2 direction = new Vector2(0, -0.4f);
+        float distance = 0.4f;
+
+        //Se dibuja el Raycast
+        Debug.DrawRay(position, direction, Color.green);
+        //Se crea el Raycast
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        //si el collider del Raycast es null isGround será verdadero de lo contrario será falso
+        isGround = (hit.collider != null) ? true : false;
+
+        if (isGround && jm.Vertical() > 0.5f)
+        {
+            //le quita la "velocidad" a la y por si la tiene
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+            //le agrega la fuerza al pet
+            rb2d.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+        }
+        else
+        {
+            return;
+        }
     }
 }
